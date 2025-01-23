a1d917648f41cd374f91a7e54c07193e2a909fa4
MdAfsarHossain <afsar291722@gmail.com>
2025-01-23 12:03:56 +0600

    Blog Title page added

---
+++ src/components/BlogTitle.jsx
@@ -0,0 +1,24 @@
+import Link from "next/link"
+
+const BlogTitle = ({ blog }) => {
+    return (
+        <div className="border-2 border-green-400/50 p-5 rounded-md flex flex-col gap-5 justify-between shadow-md hover:-translate-y-1 transition-all">
+            <h1><span className="font-bold text-lg">Blog Title:</span><br />
+                <Link
+                    href={`/blog/${blog.id}`}
+                >{blog.title}</Link></h1>
+
+            <div className="">
+                <Link
+                    href={`/blog/${blog.id}`}
+                >
+                    <button
+                        className="bg-green-500 text-white font-bold px-3 py-1 rounded-md border-2 border-green-500 hover:bg-transparent hover:text-green-500 transition-all"
+                    >View Details</button>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default BlogTitle
