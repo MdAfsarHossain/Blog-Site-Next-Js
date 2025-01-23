d7338ec1f82f1f0574605d95f698eeab621e26c3
MdAfsarHossain <afsar291722@gmail.com>
2025-01-23 12:04:16 +0600

    Footer jsx file added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <div className="flex flex-row justify-center items-center py-10 bg-gray-100">
+            <h1 className="font-bold text-lg md:text-xl lg:text-2xl text-green-500">Copyright @ 2025 || Md. Afsar Hossain</h1>
+        </div>
+    )
+}
+
+export default Footer
